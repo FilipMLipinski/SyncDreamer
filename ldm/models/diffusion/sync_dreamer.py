895e3707944b18a282611f6d1f6351fd18bbf5ce
FilipMLipinski <72980477+FilipMLipinski@users.noreply.github.com>
2024-02-05 15:52:44 +0000

    clone detach

--- ldm/models/diffusion/sync_dreamer.py
+++ ldm/models/diffusion/sync_dreamer.py
@@ -710,7 +710,7 @@ class SyncDDIMSampler:
                     if n!=anchor:
                         print("   frame: " + str(n))
                         #x_leaf = (x_prev[:, n]).clone()#.detach()
-                        x_n = x_prev[:,n]
+                        x_n = x_prev[:,n].clone().detach()
                         optimizer = torch.optim.Adam([x_n.requires_grad_()], lr=0.1)
                         print("    adam set up")
                         for i in range(3):
